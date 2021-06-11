6db4eb06cbc07bf314b341aaa180cf0d942c6d94
Islarky Overlord <islarky88@gmail.com>
2021-06-11 21:08:39 +0800

    folder structures

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import bodyParser from 'body-parser'
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.get('/', (req, res) => {
   res.send('The sedulous hyena ate the antelope!');
